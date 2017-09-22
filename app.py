17dac3c4afa84352027c789d85030e5f863be095
Quei-An Chen <kwea123@gmail.com>
2017-09-22 15:12:50 +0800

    test

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-import flask
-app = flask.Flask(__name__)
+import os
 
-@app.route("/")
-def index():
-    #do whatevr here...
-    return "Hello Heruko"
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+        return 'Hello World!'
+
+
+#if __name__ == '__main__':
+        # Bind to PORT if defined, otherwise default to 5000.
+#        port = int(os.environ.get('PORT', 5000))
+#        app.run(host='0.0.0.0', port=port)
