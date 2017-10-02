3a62b2687fb37e1eedafd87ba66703a9916d8f03
Quei-An Chen <kwea123@gmail.com>
2017-10-02 16:09:10 +0800

    slack

--- app.py
+++ app.py
@@ -16,17 +16,17 @@ def hello():
         if e['text'] == 'beauty':
             text = ""
             r = requests.get("https://www.ptt.cc/bbs/beauty/index.html")
-            soup = bs(r.text,"lxml")
-            page = soup.find("div", class_="btn-group-paging")
-            prev = page.find_all("a")[1]['href'] # previous page
-            r = requests.get("https://www.ptt.cc"+prev)
-            soup = bs(r.text,"lxml")
-            for div in soup.find_all("div", class_="r-ent"):
-                a = div.find_all("a")
-                if len(a)>0:
-                    text += "https://www.ptt.cc"+a[0]['href']+'\n'
+#             soup = bs(r.text,"lxml")
+#             page = soup.find("div", class_="btn-group-paging")
+#             prev = page.find_all("a")[1]['href'] # previous page
+#             r = requests.get("https://www.ptt.cc"+prev)
+#             soup = bs(r.text,"lxml")
+#             for div in soup.find_all("div", class_="r-ent"):
+#                 a = div.find_all("a")
+#                 if len(a)>0:
+#                     text += "https://www.ptt.cc"+a[0]['href']+'\n'
             sc.api_call("chat.postMessage",channel=e['channel'],
-            text=text)
+            text=r.text)
     return make_response("",200)
 
 if __name__ == '__main__':
