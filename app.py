42989e06530cff7cc084b93503ba04f7e18204c0
Quei-An Chen <kwea123@gmail.com>
2017-10-02 12:29:42 +0800

    slack

--- app.py
+++ app.py
@@ -6,22 +6,17 @@ from flask import Flask, request, make_response, Response
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def hello():
     sc = SlackClient(os.environ['slackbot'])
-#     for ch in sc.api_call("channels.list")['channels']:
-#         if ch['name'] == 'general':
-#             ch_general = ch['id']
-    sc.api_call(
-        "chat.postMessage",
-        channel="#general",
-        text="adsd",
-    )
-#     sc.rtm_connect()
-#     while True:
-#         for e in sc.rtm_read():
-#             if e['type'] == 'message' and e['channel'] == ch_general and 'username' not in e: # only respond to general channel and non bot users
-#                 text = ""
+    for ch in sc.api_call("channels.list")['channels']:
+        if ch['name'] == 'general':
+            ch_general = ch['id']
+    sc.rtm_connect()
+    while True:
+        for e in sc.rtm_read():
+            if e['type'] == 'message' and e['channel'] == ch_general and 'username' not in e: # only respond to general channel and non bot users
+                text = "hello"
 #                 r = requests.get("https://www.ptt.cc/bbs/beauty/index.html")
 #                 soup = bs(r.text,"lxml")
 #                 page = soup.find("div", class_="btn-group-paging")
@@ -32,9 +27,9 @@ def hello():
 #                     a = div.find_all("a")
 #                     if len(a)>0:
 #                         text += "https://www.ptt.cc"+a[0]['href']+'\n'
-#                 sc.api_call("chat.postMessage",channel=e['channel'],
-#                 text=text)
-    return "hello"
+                sc.api_call("chat.postMessage",channel=e['channel'],
+                text=text)
+    return make_response("", 200)
 
 if __name__ == '__main__':
    app.run()
