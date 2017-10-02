a0bb843c1168a967eb76e1c6ea5394e61d4bcaba
Quei-An Chen <kwea123@gmail.com>
2017-10-02 12:00:40 +0800

    slack

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ def hello():
         if ch['name'] == 'general':
             ch_general = ch['id']
     
-    sc.rtm_connect()
-    while True:
-        for e in sc.rtm_read():
-            if e['type'] == 'message' and e['channel'] == ch_general and 'username' not in e: # only respond to general channel and non bot users
-                text = ""
-                r = requests.get("https://www.ptt.cc/bbs/beauty/index.html")
-                soup = bs(r.text,"lxml")
-                page = soup.find("div", class_="btn-group-paging")
-                prev = page.find_all("a")[1]['href'] # previous page
-                r = requests.get("https://www.ptt.cc"+prev)
-                soup = bs(r.text,"lxml")
-                for div in soup.find_all("div", class_="r-ent"):
-                    a = div.find_all("a")
-                    if len(a)>0:
-                        text += "https://www.ptt.cc"+a[0]['href']+'\n'
-                sc.api_call("chat.postMessage",channel=e['channel'],
-                text=text)
-    return "OK"
+#     sc.rtm_connect()
+#     while True:
+#         for e in sc.rtm_read():
+#             if e['type'] == 'message' and e['channel'] == ch_general and 'username' not in e: # only respond to general channel and non bot users
+#                 text = ""
+#                 r = requests.get("https://www.ptt.cc/bbs/beauty/index.html")
+#                 soup = bs(r.text,"lxml")
+#                 page = soup.find("div", class_="btn-group-paging")
+#                 prev = page.find_all("a")[1]['href'] # previous page
+#                 r = requests.get("https://www.ptt.cc"+prev)
+#                 soup = bs(r.text,"lxml")
+#                 for div in soup.find_all("div", class_="r-ent"):
+#                     a = div.find_all("a")
+#                     if len(a)>0:
+#                         text += "https://www.ptt.cc"+a[0]['href']+'\n'
+#                 sc.api_call("chat.postMessage",channel=e['channel'],
+#                 text=text)
+    return sc
 
 if __name__ == '__main__':
    app.run()
