7d1bb42b277a911997b135c41722263fb257d044
Quei-An Chen <kwea123@gmail.com>
2017-10-02 15:11:19 +0800

    slack

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def hello():
     sc = SlackClient(os.environ['slackbot'])
     sc.api_call("chat.postMessage",channel='#general',
-                text=str(request.get_data()))
+                text=request.data)
 #     for ch in sc.api_call("channels.list")['channels']:
 #         if ch['name'] == 'general':
 #             ch_general = ch['id']
@@ -32,8 +32,7 @@ def hello():
 #                         text += "https://www.ptt.cc"+a[0]['href']+'\n'
 #                 sc.api_call("chat.postMessage",channel=e['channel'],
 #                 text=text)
-    body = {'challenge':'asd'}
-    return make_response("2",200)
+    return make_response(request.data['challenge'],200)
 
 if __name__ == '__main__':
    app.run()
