a680fae0691ea000aca5821b369a302d2fec2c15
Quei-An Chen <kwea123@gmail.com>
2017-10-02 13:50:23 +0800

    slack

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello():
 #         if ch['name'] == 'general':
 #             ch_general = ch['id']
     sc.api_call("chat.postMessage",channel='#general',
-                text=str(request.get_data()))
+                text=str(request.json))
 #     sc.rtm_connect()
 #     while True:
 #         for e in sc.rtm_read():
